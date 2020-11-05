344840f294dcb78f82c1f7c6bec8f606b979a85b
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2020-11-06 00:20:04 +0700

    use WorkQueue to prevent DDoS

--- express/app.js
+++ express/app.js
@@ -1,31 +1,44 @@
 const express = require('express')
 const convert = require('../lib/convert')
+const WorkQueue = require('../lib/workqueue')
+const sha1 = require('sha1')
+
+const workQueue = new WorkQueue()
 
 const routes = {
   index: (req, res) => {
     res.sendFile(__dirname + '/index.html')
   },
   convert: (req, res, next) => {
+
+    const queueKey = sha1(req.__domain + req.url)
+
     const { mimeType = 'image/jpeg', pageNumber = 1, scale = 1.0, url } = req.query
 
-    convert({
-      mimeType,
-      pageNumber: Number(pageNumber),
-      scale: Number(scale),
-      source: { url }
+    return workQueue
+    .run(queueKey, () => {
+      return convert({
+        mimeType,
+        pageNumber: Number(pageNumber),
+        scale: Number(scale),
+        source: { url }
+      })
     }).then(buffer => {
       res.type(mimeType).send(buffer)
     }).catch(error => {
       next(error)
+    }).catch(err => {
+      console.log(err)
+      res.end("Error " + err.message)
     })
   }
 }
 
 const server = express()
-  .get('/', routes.index)
-  .get('/convert', routes.convert)
-  .listen(process.env.PORT || 8080, () => {
-    const address = server.address()
-    console.info(`Ready at http://localhost:${address.port}`)
-  })
+.get('/', routes.index)
+.get('/convert', routes.convert)
+.listen(process.env.PORT || 8080, () => {
+  const address = server.address()
+  console.info(`Ready at http://localhost:${address.port}`)
+})
 
